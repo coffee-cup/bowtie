284600af9441afb340ffbb59406403e35e92c69e
Jake Runzer <jakerunzer@gmail.com>
2017-08-23 01:39:49 -0700

    add fabric

--- Scory/AppDelegate.swift
+++ Scory/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import RealmSwift
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         print("\(Realm.Configuration.defaultConfiguration.fileURL!)")
+        
+        Fabric.with([Crashlytics.self])
         
         return true
     }
